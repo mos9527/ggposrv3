08b3cd5f2b3e8e7c552b03090365fbad74fe44a5
greats3an <greats3an@gmail.com>
2021-08-29 13:21:42 +0800

    misc : removed unused imports

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-from logging import info, warn
+from logging import info, warning
 from threading import Thread
 from argparse import ArgumentParser
-import time,socket,sys,multiprocessing
+import time,socket,sys
 try:
     import coloredlogs
     coloredlogs.DEFAULT_LOG_FORMAT = '%(asctime)s [%(levelname).1s] %(name)s %(message)s'
@@ -50,7 +50,8 @@ How to access:
         info('Player server shutdown...')
         player.server.shutdown()
         info('...done...Client server shutdown...')
+        from pywebhost import __version__        
         if not client.server.shutdown():
-            warn('Shutting down forcibly')        
+            warning('Shutting down forcibly')        
         info('... All done , going home...')
         sys.exit(0)
