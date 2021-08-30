775702c4dc7df57596e6c71ed3e7d297a600c5dd
greats3an <greats3an@gmail.com>
2021-08-30 21:03:03 +0800

    web : videos as hero

--- web/src/views/Channels.vue
+++ web/src/views/Channels.vue
@@ -2,18 +2,19 @@
   <v-container class="pa-8" fluid>
     <h1 class="mb-5">{{ $t('title-channels') }}</h1>
       <v-row v-for="channel in channels" :key="channel.name">
-        <v-card style="width:100%;margin-top:1vh" :to="'/channel/?name=' + channel.name">
-          <v-img
-            :src="'/static/banners/' + channel.name + '.png'"
-            class="subheading white--text"
-            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.8)"
-            aspect-ratio="6"
-          >
-            <v-chip :key="channelsUpdate" class="ma-5 float-right" :color="channel_current == channel.name ? 'red' : 'primary'" label text-color="white">
-              {{ $t('channels-online-player-count', [channel.online]) }}
-            </v-chip>
-            <v-card-title style="margin-bottom:0" v-text="channel.desc"></v-card-title>
-          </v-img>
+        <v-card style="width:100%;margin-top:1vh;z-index:2;overflow:hidden;height:100px" :to="'/channel/?name=' + channel.name">
+          <video
+            :src="DEVELOPMENT ? '' : '/banners/' + channel.name"
+            class="hero-video" autoplay loop muted
+          >  
+           <!-- No video banners will be loaded when in development -->
+          </video>
+          <v-container class="hero-content">
+            <span class="float-left ma-4 mt-9 headline font-weight-bold" style="position:absolute" >{{channel.desc}}</span>
+            <v-chip :key="channelsUpdate" class="ma-4 mt-9 float-right" :color="channel_current == channel.name ? 'red' : 'primary'" label text-color="white">
+                {{ $t('channels-online-player-count', [channel.online]) }}
+            </v-chip>            
+          </v-container>
         </v-card>
     </v-row>
   </v-container>
@@ -23,10 +24,12 @@
 import { mapGetters } from "vuex";
 import Utils from '../common/utils';
 import { REFRESH_CHANNELS } from "../store/actions.local";
+import { DEVELOPMENT } from "../common/config"
 export default {
   data : () => ({
     channelsUpdate : 0,
-    sub : undefined
+    sub : undefined,
+    DEVELOPMENT : DEVELOPMENT
   }),
   computed: {
     ...mapGetters(["channels","channel_current"]),
